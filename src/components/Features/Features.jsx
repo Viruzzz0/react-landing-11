6e5aee80280a66f7773fb3cc788cb59e9b4b34d0
Viruz <98500701+Viruzzz0@users.noreply.github.com>
2023-07-10 21:36:37 +0000

    add viewTabs

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,7 +1,18 @@
 import css from './Features.module.css'
-import illustrationFeatures from '../../../images/illustration-features-tab-1.svg'
+import illustrationFeatures1 from '../../../images/illustration-features-tab-1.svg'
+import illustrationFeatures2 from '../../../images/illustration-features-tab-2.svg'
+import illustrationFeatures3 from '../../../images/illustration-features-tab-3.svg'
+import { useState } from 'react'
+
+const tabs = {
+  SIMPLE_BOOKMARKIN: 'SIMPLE_BOOKMARKIN',
+  SPEEDY_SEARCHING: 'SPEEDY_SEARCHING',
+  EASY_SHARING: 'EASY_SHARING'
+}
 
 function Features () {
+  const [selectTab, setSelectTab] = useState(tabs.SIMPLE_BOOKMARKIN)
+
   return (
     <section className={css.section}>
       <div className={css.conteinerText}>
@@ -11,23 +22,63 @@ function Features () {
           so you can access them on the go.
         </p>
       </div>
-      <div className={css.conteinerViewTab}>
+      <div className={css.listSelect}>
         <ul>
-          <span className={css.line} />
-          <li>Simple Bookmarking</li>
-          <span className={css.line} />
-          <li>Speedy Searching</li>
-          <span className={css.line} />
-          <li>Easy Sharing</li>
-          <span className={css.line} />
+          <li onClick={() => setSelectTab(tabs.SIMPLE_BOOKMARKIN)}>Simple Bookmarking</li>
+          <span className={css.line}>
+            <span className={selectTab === tabs.SIMPLE_BOOKMARKIN ? css.selectedlLine : ''} />
+          </span>
+          <li onClick={() => setSelectTab(tabs.SPEEDY_SEARCHING)}>Speedy Searching</li>
+          <span className={css.line}>
+            <span className={selectTab === tabs.SPEEDY_SEARCHING ? css.selectedlLine : ''} />
+          </span>
+          <li onClick={() => setSelectTab(tabs.EASY_SHARING)}>Easy Sharing</li>
+          <span className={css.line}>
+            <span className={selectTab === tabs.EASY_SHARING ? css.selectedlLine : ''} />
+          </span>
         </ul>
-        <div className={css.conteinerIllustration}>
-          <img src={illustrationFeatures} alt='' />
-          <div className={css.backgroundCard} />
-
-        </div>
       </div>
+      <Tabs selectTab={selectTab} />
     </section>
+  )
+}
+
+function Tabs ({ selectTab }) {
+  const tabContent = {
+    [tabs.SIMPLE_BOOKMARKIN]: {
+      imgSrc: illustrationFeatures1,
+      heading: 'Bookmark in one click',
+      description: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.'
+    },
+    [tabs.SPEEDY_SEARCHING]: {
+      imgSrc: illustrationFeatures2,
+      heading: 'Intelligent search',
+      description: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.'
+    },
+    [tabs.EASY_SHARING]: {
+      imgSrc: illustrationFeatures3,
+      heading: 'Share your bookmarks',
+      description: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.'
+    }
+  }
+
+  const selectedTab = tabContent[selectTab]
+
+  return (
+    <div className={css.conteinerViewTab}>
+      <div className={css.conteinerIllustration}>
+        {selectedTab && <img src={selectedTab.imgSrc} alt='' />}
+        <div className={css.backgroundCard} />
+      </div>
+      <div className={css.conteinerTextTab}>
+        {selectedTab && (
+          <>
+            <h2>{selectedTab.heading}</h2>
+            <p>{selectedTab.description}</p>
+          </>
+        )}
+      </div>
+    </div>
   )
 }
 
