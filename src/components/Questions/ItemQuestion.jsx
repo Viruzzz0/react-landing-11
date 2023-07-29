4ad5216a8497264d30aeaf9335d5a498c6700d37
viruz <nahuelsinka@gmail.com>
2023-07-29 03:47:30 -0300

    update component Questions

---
+++ src/components/Questions/ItemQuestion.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import css from './ItemQuestion.module.css'
+
+// eslint-disable-next-line react/prop-types
+function ItemQuestion ({ title, description }) {
+  const [hiddenDescription, setHiddenDescription] = useState(true)
+
+  const handleClick = () => {
+    setHiddenDescription(!hiddenDescription)
+  }
+
+  return (
+    <li className={css.question}>
+      <div className={css.headerQuestion} onClick={handleClick}>
+        {title}
+        <button>
+          <svg width='30px' height='30px' viewBox='0 0 24.00 24.00' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <g id='SVGRepo_bgCarrier' strokeWidth='0' />
+            <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' />
+            <g id='SVGRepo_iconCarrier'>
+              <path d='M7 10L12 15L17 10' stroke='#5266df' strokeWidth='1.8240000000000003' strokeLinecap='round' strokeLinejoin='round' />
+            </g>
+          </svg>
+        </button>
+      </div>
+
+      <div className={`${css.description} ${hiddenDescription === true ? css.hidden : ''}`}>
+        {description}
+      </div>
+
+      <span className={css.line} />
+    </li>
+  )
+}
+export default ItemQuestion
--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import ItemQuestion from './ItemQuestion'
 import css from './Questions.module.css'
 
 function Questions () {
+  const questions = [
+    {
+      title: 'What is Bookmark?',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.'
+    },
+    {
+      title: 'How can I request a new browser?',
+      description: 'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.'
+    },
+    {
+      title: 'Is there a mobile app?',
+      description: 'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.'
+    }, {
+      title: 'What about other Chromium browsers?',
+      description: 'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.'
+    }
+
+  ]
+
   return (
     <section className={css.section}>
       <div className={css.containerDescription}>
-        <h3>Frequently Asked Questions</h3>
-        <p>Here are some of our FAQs. If you have any other questions
-          you'd like answered please feel free to email us.
+        <h2>Frequently Asked Questions</h2>
+        <p>{`Here are some of our FAQs. If you have any other questions
+          you'd like answered please feel free to email us.`}
         </p>
       </div>
       <div className={css.containerQuestion}>
-        <ul>
-          <li>What is Bookmark?</li>
-          <li>How can I request a new browser?</li>
-          <li>Is there a mobile app?</li>
-          <li>What about other Chromium browsers?</li>
+        <ul className={css.listQuestion}>
+          {
+            questions.map(({ title, description }, index) => (
+              <ItemQuestion key={index} title={title} description={description} />
+            ))
+          }
         </ul>
       </div>
     </section>
